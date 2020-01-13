d41610bf81df90c453446332c20e0087bb608b5c
Saulo Pratti <blude@me.com>
2020-01-13 23:18:06 +0100

    refactored version with more concise code

--- grade-school/Sources/GradeSchool/GradeSchool.swift
+++ grade-school/Sources/GradeSchool/GradeSchool.swift
@@ -1,23 +1,15 @@
-struct GradeSchool {
+class GradeSchool {
     var roster: [Int: [String]] = [:]
-    lazy var sortedRoster: [Int:[String]] = {
-        var sorted: [Int: [String]] = [:]
-        for key in roster.keys {
-            sorted.updateValue(roster[key]?.sorted() ?? [String](), forKey: key)
-        }
-        return sorted
-    }()
+
+    var sortedRoster: [Int: [String]] {
+        roster.mapValues { $0.sorted() }
+    }
 
     func studentsInGrade(_ grade: Int) -> [String] {
-        guard let students = roster[grade] else { return [String]() }
-        return students
+        roster[grade, default: []]
     }
     
-    mutating func addStudent(_ name: String, grade: Int) {
-        if roster[grade] == nil {
-            roster[grade] = [name]
-        } else {
-            roster[grade]?.append(name)
-        }
+    func addStudent(_ name: String, grade: Int) {
+        roster[grade, default: []].append(name)
     }
 }
