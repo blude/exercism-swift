47507d6a7f426b5166276a52a1a11dc24e277e7e
Saulo Pratti <blude@me.com>
2020-02-26 00:29:35 +0100

    initial solution with several failing tests

--- scale-generator/Sources/ScaleGenerator/ScaleGenerator.swift
+++ scale-generator/Sources/ScaleGenerator/ScaleGenerator.swift
@@ -1,19 +1,89 @@
-struct ScaleGenerator {
-    var tonic: String
-    var scaleName: String
-    var pattern: String
-
-    var name: String {
-        return "\(tonic.uppercased()) \(scaleName)"
+extension String {
+    func capitalizing() -> String {
+        prefix(1).uppercased() + self.lowercased().dropFirst()
     }
+}
+
+class ScaleGenerator {
+    let scaleName: String
+    let tonic: String
+    let pattern: String
     
-    func pitches() -> [String] {
-        return [""]
+    let chromaticSharps = ["A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"]
+    let chromaticFlats =  ["A", "Bb", "B", "C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab"]
+    
+    let noAccidentals = ["A minor", "C major"]
+    let useSharps = [
+        "G major", "D major", "A major", "E major", "B major", "F# major",
+        "e minor", "b minor", "f# minor", "c# minor", "g# minor", "d# minor"
+    ]
+    let useFlats = [
+        "F major", "Bb major", "Eb major", "Ab major", "Db major", "Gb major",
+        "d minor", "g minor", "c minor", "f minor", "bb minor", "eb minor"
+    ]
+    
+    var steps: [Character: Int] = [
+        "m": 1,
+        "M": 2,
+        "A": 3
+    ]
+    
+    var name: String {
+        "\(tonic.uppercased()) \(scaleName)"
     }
     
     init(tonic: String, scaleName: String, pattern: String = "") {
-        self.tonic = tonic
         self.scaleName = scaleName
+        self.tonic = tonic
         self.pattern = pattern
+    }
+    
+    func pitches() -> [String] {
+        var pitches: [String] = []
+        guard let startIndex = chromaticSharps.index(of: tonic.capitalizing()) else { return pitches }
+
+        switch scaleName {
+        case "chromatic":
+            var noteIndex = startIndex
+            for _ in 0 ..< 12 {
+                pitches.append(chromaticSharps[noteIndex])
+                if noteIndex >= chromaticSharps.count - 1 {
+                    noteIndex = 0
+                } else {
+                    noteIndex += 1
+                }
+            }
+        case "major":
+            var noteIndex = startIndex
+            for letter in pattern {
+                pitches.append(chromaticSharps[noteIndex])
+                if noteIndex >= chromaticSharps.count - steps[letter]! {
+                    noteIndex = 0
+                } else {
+                    noteIndex += steps[letter]!
+                }
+            }
+        case "minor":
+            var noteIndex = startIndex
+            for letter in pattern {
+                pitches.append(chromaticFlats[noteIndex])
+                if noteIndex >= chromaticFlats.count - steps[letter]! {
+                    noteIndex = 0
+                } else {
+                    noteIndex += steps[letter]!
+                }
+            }
+        default:
+            var noteIndex = startIndex
+            for letter in pattern {
+                pitches.append(chromaticSharps[noteIndex])
+                if noteIndex >= chromaticSharps.count - steps[letter]! {
+                    noteIndex = 0
+                } else {
+                    noteIndex += steps[letter]!
+                }
+            }
+        }
+        return pitches
     }
 }
