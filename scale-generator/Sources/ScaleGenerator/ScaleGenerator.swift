ccb5f2e6c321a7572562b8f3e5002e7d791384a8
Saulo Pratti <blude@me.com>
2020-02-26 01:09:38 +0100

    refactoring to fix failing tests

--- scale-generator/Sources/ScaleGenerator/ScaleGenerator.swift
+++ scale-generator/Sources/ScaleGenerator/ScaleGenerator.swift
@@ -5,84 +5,45 @@ extension String {
 }
 
 class ScaleGenerator {
-    let scaleName: String
-    let tonic: String
-    let pattern: String
+    let name: String
+    private let tonic: String
+    private let pattern: String
     
-    let chromaticSharps = ["A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"]
-    let chromaticFlats =  ["A", "Bb", "B", "C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab"]
+    private let usesSharps: Bool
+    private let chromaticSharps = ["A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"]
+    private let chromaticFlats =  ["A", "Bb", "B", "C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab"]
     
-    let noAccidentals = ["A minor", "C major"]
-    let useSharps = [
-        "G major", "D major", "A major", "E major", "B major", "F# major",
-        "e minor", "b minor", "f# minor", "c# minor", "g# minor", "d# minor"
-    ]
-    let useFlats = [
-        "F major", "Bb major", "Eb major", "Ab major", "Db major", "Gb major",
-        "d minor", "g minor", "c minor", "f minor", "bb minor", "eb minor"
-    ]
-    
-    var steps: [Character: Int] = [
+    private let steps: [Character: Int] = [
         "m": 1,
         "M": 2,
         "A": 3
     ]
     
-    var name: String {
-        "\(tonic.uppercased()) \(scaleName)"
-    }
-    
-    init(tonic: String, scaleName: String, pattern: String = "") {
-        self.scaleName = scaleName
-        self.tonic = tonic
+    init(tonic: String, scaleName: String, pattern: String = "mmmmmmmmmmmm") {
+        self.tonic = tonic.capitalizing()
+        self.name = "\(self.tonic) \(scaleName.lowercased())"
         self.pattern = pattern
+        
+        switch (tonic, scaleName) {
+        case ("F", _), ("Bb", _), ("Eb", _), ("Ab", _), ("Db", _), ("Gb", "major"), ("d", _), ("g", _), ("c", _), ("f", _), ("bb", _), ("eb", "minor"):
+            usesSharps = false
+        default:
+            usesSharps = true
+        }
     }
     
     func pitches() -> [String] {
-        var pitches: [String] = []
-        guard let startIndex = chromaticSharps.index(of: tonic.capitalizing()) else { return pitches }
+        let scale = usesSharps ? chromaticSharps : chromaticFlats
 
-        switch scaleName {
-        case "chromatic":
-            var noteIndex = startIndex
-            for _ in 0 ..< 12 {
-                pitches.append(chromaticSharps[noteIndex])
-                if noteIndex >= chromaticSharps.count - 1 {
-                    noteIndex = 0
-                } else {
-                    noteIndex += 1
-                }
-            }
-        case "major":
-            var noteIndex = startIndex
-            for letter in pattern {
-                pitches.append(chromaticSharps[noteIndex])
-                if noteIndex >= chromaticSharps.count - steps[letter]! {
-                    noteIndex = 0
-                } else {
-                    noteIndex += steps[letter]!
-                }
-            }
-        case "minor":
-            var noteIndex = startIndex
-            for letter in pattern {
-                pitches.append(chromaticFlats[noteIndex])
-                if noteIndex >= chromaticFlats.count - steps[letter]! {
-                    noteIndex = 0
-                } else {
-                    noteIndex += steps[letter]!
-                }
-            }
-        default:
-            var noteIndex = startIndex
-            for letter in pattern {
-                pitches.append(chromaticSharps[noteIndex])
-                if noteIndex >= chromaticSharps.count - steps[letter]! {
-                    noteIndex = 0
-                } else {
-                    noteIndex += steps[letter]!
-                }
-            }
+        guard var index = scale.firstIndex(of: tonic) else {
+            fatalError("Invalid index")
+        }
+
+        var pitches = [tonic]
+
+        for step in pattern.dropLast() {
+            index = (index + steps[step]!) % 12
+            pitches.append(scale[index])
         }
         return pitches
     }
