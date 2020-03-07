d149f7fb946f6aabc21bfa32499ed4e74c4cc5a8
Saulo Pratti <blude@me.com>
2020-03-07 19:48:12 +0100

    add initial solution

---
+++ clock/Sources/Clock/Clock.swift
@@ -0,0 +1,46 @@
+import Foundation
+
+class Clock: Equatable {
+    var time: Date
+
+    init(hours: Int = 0, minutes: Int = 0) {
+        var components = DateComponents()
+        components.hour = hours
+        components.minute = minutes
+        
+        guard let timeSet = Calendar.current.date(from: components) else { fatalError() }
+        time = timeSet
+    }
+    
+    func add(minutes: Int) -> Clock {
+        var components = DateComponents()
+        components.hour = 0
+        components.minute = minutes
+        
+        guard let timeSet = Calendar.current.date(byAdding: components, to: time) else { fatalError() }
+        time = timeSet
+        return self
+    }
+    
+    func subtract(minutes: Int) -> Clock {
+        var components = DateComponents()
+        components.hour = 0
+        components.minute = minutes * -1
+
+        guard let timeSet = Calendar.current.date(byAdding: components, to: time) else { fatalError() }
+        time = timeSet
+        return self
+    }
+
+    static func == (lhs: Clock, rhs: Clock) -> Bool {
+        lhs.description == rhs.description
+    }
+}
+
+extension Clock: CustomStringConvertible {
+    var description: String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "HH:mm"
+        return formatter.string(from: time)
+    }
+}
