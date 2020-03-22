72fc9811d4e82a79a821dc832e2ea8c2221fb629
Saulo Pratti <blude@me.com>
2020-03-22 23:42:59 +0100

    simplify code

--- isbn-verifier/Sources/IsbnVerifier/IsbnVerifier.swift
+++ isbn-verifier/Sources/IsbnVerifier/IsbnVerifier.swift
@@ -6,34 +6,21 @@ class IsbnVerifier {
         
         guard isbnDigits.count == 10 else { return false }
         
-        var digits = [Int]()
         var sum = 0
 
         for (index, digit) in isbnDigits.enumerated() {
-            if digit.isLetter && digit != "X" {
+            if digit == "X" {
+                if index != 9 {
+                    return false
+                }
+                sum += 10
+            } else if digit.isLetter {
                 return false
-            } else if digit == "X" && index != 9 {
-                return false
-            } else if digit == "X" {
-                digits.append(10)
             } else {
-                digits.append(Int(String(digit))!)
+                sum += Int(String(digit))! * (10 - index)
             }
         }
-
-        sum += digits[0] * 10
-        sum += digits[1] * 9
-        sum += digits[2] * 8
-        sum += digits[3] * 7
-        sum += digits[4] * 6
-        sum += digits[5] * 5
-        sum += digits[6] * 4
-        sum += digits[7] * 3
-        sum += digits[8] * 2
-        sum += digits[9] * 1
-
-        guard sum % 11 == 0 else { return false }
         
-        return true
+        return sum % 11 == 0
     }
 }
